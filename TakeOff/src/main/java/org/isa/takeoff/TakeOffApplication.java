a828cde3b87c8444318ef0c39b91534f97e58009
mare11 <44523703+mare11@users.noreply.github.com>
2018-11-15 20:36:47 +0100

    First project commit
    Co-Authored-By: krspogacin <krspogacin@users.noreply.github.com>
    Co-Authored-By: duca43 <duca43@users.noreply.github.com>

---
+++ TakeOff/src/main/java/org/isa/takeoff/TakeOffApplication.java
@@ -0,0 +1,12 @@
+package org.isa.takeoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeOffApplication.class, args);
+	}
+}
---
+++ TakeOff/src/test/java/org/isa/TakeOff/TakeOffApplicationTests.java
@@ -0,0 +1,16 @@
+package org.isa.TakeOff;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TakeOffApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
